811e7730f3a0eb86d09b7457591e0da3fb46206c
zxy <21228@qq.com>
2025-11-05 18:18:53 +0800

    更新管理员界面配置

--- passengers/admin.py
+++ passengers/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Passenger
 
-# Register your models here.
+
+@admin.register(Passenger)
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'phone_number']
+    list_display_links = ['id', 'first_name']
+    search_fields = ['first_name', 'last_name', 'email', 'phone_number']
+    list_filter = ['first_name', 'last_name']
+    list_per_page = 20
+
+
+
--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    # 列表页显示的字段
+    list_display = ['booking_reference', 'flight', 'passenger', 'seat_number', 'price', 'status']
+
+    # 可以点击的字段（链接到编辑页）
+    list_display_links = ['booking_reference']
+
+    # 搜索字段
+    search_fields = ['booking_reference', 'seat_number', 'passenger__first_name', 'passenger__last_name',
+                     'flight__flight_number']
+
+    # 右侧过滤器
+    list_filter = ['status', 'flight', 'passenger']
+
+    # 每页显示的项目数
+    list_per_page = 20
+
+    # 默认排序
+    ordering = ['-id']
